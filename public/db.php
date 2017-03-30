b80b02534f593ac0e67d1c7af7cd7174d2b2f7fc
Peter Chow <peteroid.howinai@gmail.com>
2017-03-30 10:48:28 +0800

    update routes; add close

--- public/db.php
+++ public/db.php
@@ -2,8 +2,15 @@
 
 class MockDB {
   var $filename = __DIR__ . '/db.store';
+  var $message = '';
 
-  // simulate db open action
+  /**
+   * Simulate db open action
+   * Create an account by account id with the params
+   * and store the account
+   * @param $accountId : string
+   * @param $param - other account details : array
+   */
   public function open($accountId, $param = []) {
     $account = null;
     $opened = false;
@@ -14,19 +21,42 @@ class MockDB {
           'created_date' => time()
         ));
         $opened = true;
+      } else {
+        $this->message = "Account already exists";
       }
 
       $account = $accounts[$accountId];
-      return $accounts;
     });
 
-    return array(
+    return $this->withMessage(array_merge(array(
       'opened' => $opened,
+    ), $opened ? array(
       'account' => $account
-    );
+    ) : array()));
+  }
+
+  /** db close action
+   * Delete an account by account id from the store
+   * @param $accountId : string
+   */
+  public function close($accountId) {
+    $deleted = false;
+    $this->withStore(function (&$accounts) use ($accountId, &$deleted) {
+      if (array_key_exists($accountId, $accounts)) {
+        unset($accounts[$accountId]);
+        $deleted = !array_key_exists($accountId, $accounts);
+      } else {
+        $this->message = "Account doesn't exist";
+      }
+    });
+
+    return $this->withMessage(array(
+      'deleted' => $deleted
+    ));
   }
 
   // wrapper for getting the store and then store it back after action
+  // this is just a simple json store, not using for real production usage
   private function withStore($cb) {
     // retrive the store
     $accounts = json_decode(file_get_contents($this->filename), true);
@@ -41,6 +71,12 @@ class MockDB {
     // save the store
     file_put_contents($this->filename, json_encode($accounts));
     // error_log(json_encode($accounts));
+  }
+
+  private function withMessage($responseParam) {
+    return ($this->message !== '') ? array_merge($responseParam, array(
+      'message' => $this->message
+    )) : $responseParam;
   }
 }
 
--- public/index.php
+++ public/index.php
@@ -11,16 +11,24 @@ $app = new \Slim\App(['settings' => [
 $db = new MockDB();
 
 // open account
-$app->get('/{accountId}/open', function ($request, $response, $args) use ($db) {
+$app->post('/{accountId}/open', function ($request, $response, $args) use ($db) {
   $dbResponse = $db->open($args['accountId']);
   return $response->withJson($dbResponse);
 });
 
-// // close account
-// $app->get('/{accountId}/open', function ($request, $response, $args) use ($db) {
-//   $dbResponse = $db->open($args['accountId']);
-//   return $response->withJson($dbResponse);
-// });
+// close account
+$app->post('/{accountId}/close', function ($request, $response, $args) use ($db) {
+  $dbResponse = $db->close($args['accountId']);
+  return $response->withJson($dbResponse);
+});
+
+// TODO
+// get balance
+$app->get('/{accountId}/balance', function ($request, $response, $args) use ($db) {});
+
+// TODO
+// withdraw and deposit money
+$app->post('/{accountId}/balance', function ($request, $response, $args) use ($db) {});
 
 // Run app
 $app->run();
